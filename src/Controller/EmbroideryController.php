5888e2d8868ad23b7125b717f678841291f68831
Barbara Gonthier <barbara.gonthier@gmail.com>
2020-11-02 17:56:41 +0100

    added embroideryManager, modified controller and view accordingly, adjusted css

--- src/Controller/EmbroideryController.php
+++ src/Controller/EmbroideryController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-use FilesystemIterator;
+use App\Model\EmbroideryManager;
 
 class EmbroideryController extends AbstractController
 {
     public function examples()
     {
-        $directoryPath = 'assets/images/embroidery-examples';
-        $files = new FilesystemIterator($directoryPath, FilesystemIterator::SKIP_DOTS);
-        return $this->twig->render('Embroidery/embroidery-examples.html.twig', ['files' => $files]);
+        $embroideryManager = new EmbroideryManager();
+        $embroideries = $embroideryManager->selectAll();
+        return $this->twig->render('Embroidery/embroidery-examples.html.twig', ['embroideries' => $embroideries]);
     }
 }
