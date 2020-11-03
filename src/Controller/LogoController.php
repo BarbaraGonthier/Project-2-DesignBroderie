ad096f052f09bc78ba5d874c97453db8a2d37115
Barbara Gonthier <barbara.gonthier@gmail.com>
2020-11-03 15:31:12 +0100

    created logocontroller

---
+++ src/Controller/LogoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\LogoManager;
+
+class LogoController extends AbstractController
+{
+    public function show()
+    {
+        $logoManager = new LogoManager();
+        $logos = $logoManager->selectAll();
+        return $this->twig->render('Logos/logo-examples.html.twig', ['logos' => $logos]);
+    }
+}
