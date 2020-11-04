f4893c7124bcb75f46c09fe9c5afc808ac9bad87
calouvts <pascalevetois@gmail.com>
2020-11-04 11:40:55 +0100

    update ProductController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -13,20 +13,4 @@ class ProductController extends AbstractController
         return $this->twig->render('Productadmin/index.html.twig', ['products' => $products]);
     }
 
-    /**
-     * Display item informations specified by $id
-     *
-     * @param int $id
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
-    public function show(int $id)
-    {
-        $productManager = new ProductManager();
-        $product = $productManager->selectOneById($id);
-
-        return $this->twig->render('Productadmin/show.html.twig', ['product' => $product]);
-    }
 }
