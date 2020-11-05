d075ed90700e38faa3e9def8c2e287c5311cfcd5
Barbara Gonthier <barbara.gonthier@gmail.com>
2020-11-05 14:40:24 +0100

    added category manager and controller

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\CategoryManager;
+use App\Model\ProductManager;
+
+class CategoryController extends AbstractController
+{
+    public function list(int $categoryId)
+    {
+        $categoryManager = new CategoryManager();
+        $categories = $categoryManager->selectAll();
+        $productManager = new ProductManager();
+        $products = $productManager->selectAllByCategoryId($categoryId);
+        return $this->twig->render(
+            'Product/productByCategory.html.twig',
+            ['products' => $products, 'categories' => $categories]
+        );
+    }
+}
