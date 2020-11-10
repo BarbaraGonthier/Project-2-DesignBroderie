00132f99d40a456c8507ab0ec68092d4c9b56ca0
calouvts <pascalevetois@gmail.com>
2020-11-10 12:25:31 +0100

    add product admin

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Model\ProductManager;
+use App\Model\CategoryManager;
 
 class ProductController extends AbstractController
 {
     /**
-     * Display item informations specified by $id
+     * Display product informations specified by $id
      *
      * @param int $id
      * @return string
@@ -22,10 +23,81 @@ class ProductController extends AbstractController
 
         return $this->twig->render('Productadmin/show.html.twig', ['product' => $product]);
     }
+
     public function index()
     {
         $productManager = new ProductManager();
         $products = $productManager->selectAll();
         return $this->twig->render('Productadmin/index.html.twig', ['products' => $products]);
+    }
+
+    /**
+     * Display product creation page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function add()
+    {
+        $categoryManager = new CategoryManager();
+        $categories = $categoryManager->selectAll();
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $product = array_map('trim', $_POST);
+            $errors = $this->productValidation($product);
+            if (empty($errors)) {
+                $productManager = new ProductManager();
+                $id = $productManager->insert($product);
+                header('Location:/product/show/' . $id);
+                return "votre produit a été rajouté";
+            }
+        }
+        return $this->twig->render('Productadmin/add.html.twig', [
+            'errors' => $errors ?? [],
+            'product' => $product ?? [],
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @param array $product
+     * @return array
+     * @SuppressWarnings(PHPMD)
+     */
+
+    private function productValidation(array $product): array
+    {
+        $errors = [];
+
+        if (empty($product['name'])) {
+            $errors[] = 'Le nom du produit doit être complété';
+        }
+        $length = 100;
+        if (!empty($product['name']) && strlen($product['name']) > $length) {
+            $errors[] = 'Le nom du produit doit contenir moins de ' . $length . ' caractères';
+        }
+
+        if (empty($product['gender'])) {
+            $errors[] = 'Le genre doit être complété';
+        }
+        if (!empty($product['gender']) && strlen($product['name']) > $length) {
+            $errors[] = 'Le genre doit contenir moins de' . $length . ' caractères';
+        }
+
+        if (empty($product['reference'])) {
+            $errors[] = 'La référence du produit doit être complétée';
+        }
+        if (!empty($product['reference']) && strlen($product['name']) > $length) {
+            $errors[] = 'La référence du produit doit contenir moins de' . $length . ' caractères';
+        }
+        if (empty($product['price'])) {
+            $errors[] = 'Le prix doit être complété';
+        }
+        if (!empty($product['price']) && $product['price'] < 0) {
+            $errors[] = 'Le prix ne peut être négatif';
+        }
+        return $errors ?? [];
     }
 }
--- src/Model/ProductManager.php
+++ src/Model/ProductManager.php
@@ -18,4 +18,34 @@ class ProductManager extends AbstractManager
 
         return $statement->fetchAll();
     }
+
+    public function selectWithCategory()
+    {
+        return $this->pdo->query(
+            'SELECT p.*, c.name as category ' .
+            'FROM ' . self::TABLE . ' p ' .
+            'LEFT JOIN category c ON p.category_id=c.id'
+        )->fetchAll();
+    }
+
+
+    public function insert(array $product): int
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`category` , `name` , `gender` , 
+            `reference` , `image` , `description` , `price`) 
+            VALUES (:category , :name , :gender , :reference, :image ,:description, :price)");
+        $statement->bindValue('category', $product['category']);
+        $statement->bindValue('name', $product['name'], \PDO::PARAM_STR);
+        $statement->bindValue('gender', $product['gender'], \PDO::PARAM_STR);
+        $statement->bindValue('reference', $product['reference'], \PDO::PARAM_INT);
+        $statement->bindValue('image', $product['image'], \PDO::PARAM_LOB);
+        $statement->bindValue('description', $product['description'], \PDO::PARAM_STR);
+        $statement->bindValue('price', $product['price']);
+
+
+        if ($statement->execute()) {
+            return (int)$this->pdo->lastInsertId();
+        }
+    }
 }
