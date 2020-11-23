320408891b2946aa537c54906982c2286f6241c9
Kronigeur <loicchenuet@hotmail.fr>
2020-11-23 09:16:53 +0100

    fix erreur git

--- src/Model/CategoryManager.php
+++ src/Model/CategoryManager.php
@@ -2,9 +2,12 @@
 
 namespace App\Model;
 
+use PDO;
+
 class CategoryManager extends AbstractManager
 {
     public const TABLE = 'category';
+
     /**
      *  Initializes this class.
      */
@@ -16,13 +19,13 @@ class CategoryManager extends AbstractManager
     public function delete(int $id): void
     {
         $statement = $this->pdo->prepare("DELETE FROM " . self::TABLE . " WHERE id=:id");
-        $statement->bindValue(':id', $id, \PDO::PARAM_INT);
-
+        $statement->bindValue(':id', $id, PDO::PARAM_INT);
+    }
     public function insert(array $category)
     {
         $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, image) VALUES (:name, :image)");
-        $statement->bindValue('name', $category['name'], \PDO::PARAM_STR);
-        $statement->bindValue('image', $category['image'], \PDO::PARAM_STR);
+        $statement->bindValue('name', $category['name'], PDO::PARAM_STR);
+        $statement->bindValue('image', $category['image'], PDO::PARAM_STR);
 
         $statement->execute();
     }
